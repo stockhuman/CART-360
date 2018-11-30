75b9c27134483a5d7b50d95d27f2bcb3357b6c74
Michael Hemingway <mpghemingway@gmail.com>
2018-11-29 20:08:47 -0500

    Audio effects funtional

--- ASSIGNMENTS/THEN/v2/src/ESP8266/ESP8266.ino
+++ ASSIGNMENTS/THEN/v2/src/ESP8266/ESP8266.ino
@@ -116,3 +116,90 @@ void loop() {
   Serial.println("");
 
 }
+
+// #include <ESP8266WiFi.h>
+// #include <WiFiClient.h>
+// #include <ESP8266WebServer.h>
+// #include <WebSocketsServer.h>
+// #include <Hash.h>
+
+// // Replace with your network credentials
+// const char *ssid = "ConcordiaUniversity";
+// const char *password = "<YOUR WIFI PASSWORD>";
+
+// WebSocketsServer webSocket = WebSocketsServer(81);
+// ESP8266WebServer server(80); // instantiate server at port 80 (http port)
+
+// String page = "";
+// int LEDPin = LED_BUILTIN;
+
+// void setup(void)
+// {
+//   //the HTML of the web page
+//   page = "<h1>Simple NodeMCU Web Server</h1><p><a href=\"LEDOn\"><button>ON</button></a>&nbsp;<a href=\"LEDOff\"><button>OFF</button></a></p>";
+//   //make the LED pin output and initially turned off
+//   pinMode(LEDPin, OUTPUT);
+//   digitalWrite(LEDPin, LOW);
+
+//   delay(1000);
+
+//   Serial.begin(115200);
+//   WiFi.begin(ssid, password); // begin WiFi connection
+//   Serial.println("");
+
+//   // Wait for connection
+//   while (WiFi.status() != WL_CONNECTED)
+//   {
+//     delay(500);
+//     Serial.print(".");
+//   }
+
+//   Serial.println("");
+//   Serial.print("Connected to ");
+//   Serial.println(ssid);
+//   Serial.print("IP address: ");
+//   Serial.println(WiFi.localIP());
+
+//   server.on("/", []() {
+//     server.send(200, "text/html", page);
+//   });
+
+//   server.on("/LEDOn", []() {
+//     server.send(200, "text/html", page);
+//     digitalWrite(LEDPin, HIGH);
+//     delay(1000);
+//   });
+
+//   server.on("/LEDOff", []() {
+//     server.send(200, "text/html", page);
+//     digitalWrite(LEDPin, LOW);
+//     delay(1000);
+//   });
+
+//   server.begin();
+//   webSocket.begin();
+//   webSocket.onEvent(webSocketEvent);
+
+//   Serial.println("Web server started!");
+// }
+
+// void loop(void)
+// {
+//   webSocket.loop();
+//   server.handleClient();
+//   if (Serial.available() > 0)
+//   {
+//     char c[] = {(char)Serial.read()};
+//     webSocket.broadcastTXT(c, sizeof(c));
+//   }
+// }
+
+// void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+// {
+//   if (type == WStype_TEXT)
+//   {
+//     for (int i = 0; i < length; i++)
+//       Serial.print((char)payload[i]);
+//     Serial.println();
+//   }
+// }
--- ASSIGNMENTS/THEN/v2/src/main.cpp
+++ ASSIGNMENTS/THEN/v2/src/main.cpp
@@ -1,47 +1,54 @@
 #include <Audio.h>
-
-#include <Arduino.h>
 #include <SD.h>
 #include <SD_t3.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <ssd1351.h>
-#define SLOW_SPI
+
 #define SDCARD_CS_PIN BUILTIN_SDCARD
 #define SDCARD_MOSI_PIN 7
 #define SDCARD_SCK_PIN 14
 
-typedef ssd1351::LowColor Color;
-
-// Choose display buffering - NoBuffer or SingleBuffer currently supported
-// auto display = ssd1351::SSD1351<Color, ssd1351::NoBuffer, 128, 96>();
-auto display = ssd1351::SSD1351<Color, ssd1351::SingleBuffer, 128, 96>();
-
-bool up = false;
-int pos = 127;
-const int particles = 256;
-int offsets[particles];
-int x_pos[particles];
-int y_pos[particles];
-Color particle_colors[particles];
-
 // Audio IO
-AudioOutputI2S i2s_out;
-AudioInputAnalogStereo adc_in;
-AudioConnection audioL(adc_in, 0, i2s_out, 0);
-AudioConnection audioR(adc_in, 1, i2s_out, 1);
+// AudioOutputI2S i2s_out;
+// AudioInputAnalogStereo adc_in;
+// AudioConnection audioL(adc_in, 0, i2s_out, 0);
+// AudioConnection audioR(adc_in, 1, i2s_out, 1);
+
+// GUItool: begin automatically generated code
+AudioInputAnalogStereo adc_in;  //xy=62,267.3666687011719
+AudioFilterBiquad filterR;      //xy=254.36666870117188,371.3666687011719
+AudioFilterBiquad filterL;      //xy=269.3666687011719,140.36666870117188
+AudioEffectFreeverb fx_reverbL; //xy=464.3666687011719,41.366668701171875
+AudioEffectDelay fx_delayL;     //xy=474.3666687011719,134.36666870117188
+AudioEffectDelay fx_delayR;     //xy=479.3666687011719,486.3666687011719
+AudioEffectFreeverb fx_reverbR; //xy=488.3666687011719,366.3666687011719
+AudioMixer4 mixerR;             //xy=693.36669921875,342.3666687011719
+AudioMixer4 mixerL;             //xy=695.36669921875,205.36666870117188
+AudioOutputI2S i2s_out;         //xy=920.4334716796875,265.5697326660156
+AudioConnection patchCord1(adc_in, 0, mixerL, 0);
+AudioConnection patchCord2(adc_in, 0, filterL, 0);
+AudioConnection patchCord3(adc_in, 1, mixerR, 0);
+AudioConnection patchCord4(adc_in, 1, filterR, 0);
+AudioConnection patchCord5(filterR, fx_delayR);
+AudioConnection patchCord6(filterR, fx_reverbR);
+AudioConnection patchCord7(filterL, fx_delayL);
+AudioConnection patchCord8(filterL, fx_reverbL);
+AudioConnection patchCord9(fx_reverbL, 0, mixerL, 1);
+AudioConnection patchCord10(fx_delayL, 0, mixerL, 2);
+AudioConnection patchCord11(fx_delayR, 0, mixerR, 2);
+AudioConnection patchCord12(fx_reverbR, 0, mixerR, 1);
+AudioConnection patchCord13(mixerR, 0, i2s_out, 1);
+AudioConnection patchCord14(mixerL, 0, i2s_out, 0);
+// GUItool: end automatically generated code
 
 // Audio Effects
-AudioEffectEnvelope env;
-int noteTimeout = 2000;
-int envelopeButton = 0;
 
 // SD card playback
 AudioPlaySdWav audioSD;
 AudioRecordQueue queue;
-AudioConnection patchCord3(adc_in, 0, queue, 0);
-AudioConnection patchCord4(audioSD, 0, i2s_out, 0);
-AudioConnection patchCord5(audioSD, 0, i2s_out, 1);
+AudioConnection patch_queue(adc_in, 0, queue, 0);
+AudioConnection patch_SDOutL(audioSD, 0, i2s_out, 0);
+AudioConnection patch_SDOutR(audioSD, 0, i2s_out, 1);
 
 // SD Card variables
 unsigned long ChunkSize = 0L;
@@ -66,16 +73,39 @@ elapsedMillis msecs;
 
 void setup()
 {
+  // Communication between MCUs
   Serial.begin(115200);  // Debug w/ PC
   Serial1.begin(115200); // Communication with ESP2688
-  display.begin();
-  for (int i = 0; i < particles; i++) {
-    x_pos[i] = random(0, 128);
-    y_pos[i] = random(0, 96);
-    particle_colors[i] = ssd1351::RGB(0, i + 10, i / 2 + 10);
-  }
+
+  // Audio Effects
+  fx_reverbL.roomsize(0.5); // Seconds, not millis
+  fx_reverbL.damping(0.3);
+  fx_reverbR.roomsize(0);
+  fx_reverbR.damping(1);
+
+  fx_delayL.delay(0, 10); // channel, millis
+  fx_delayR.delay(0, 0);    // channel, millis
+  fx_delayL.disable(2); // disables taps
+
+  //set xbitDepth to 16 and xsampleRate to 44100 for passthru
+  // bitcrusher.bits(16); // passthrough
+  // ampL.gain(0.2);
+  // ampR.gain(0.2);
+  filterR.setLowpass(0, 700);
+  filterL.setLowpass(0, 900);
+
+  mixerL.gain(0, 0);
+  mixerL.gain(1, 1);
+  mixerL.gain(2, 0);
+
+  mixerR.gain(0, 0);
+  mixerR.gain(1, 0.5);
+  mixerR.gain(2, 0.5);
+
+  // AudioInterrupts();
   AudioMemory(60);
 
+  // SD Card
   SPI.setMOSI(SDCARD_MOSI_PIN);
   SPI.setSCK(SDCARD_SCK_PIN);
   if (!(SD.begin(SDCARD_CS_PIN)))
@@ -89,6 +119,7 @@ void setup()
   }
 }
 
+// SD Card fns, see sd_card.cpp
 extern void startRecording();
 extern void continueRecording();
 extern void writeOutHeader();
@@ -96,44 +127,21 @@ extern void stopRecording();
 extern void startPlaying();
 extern void stopPlaying();
 
+// WiFi control functions
+
+
+// foward declarations for the loop
+void _loop_interactionSD();
+void _loop_interactionWifi();
+void controlGrain(int, float);
+
 void loop() {
+  _loop_interactionWifi();
+  _loop_interactionSD();
+}
 
-  unsigned long before = millis();
-  display.fillScreen(ssd1351::RGB());
-  Color circleColor = ssd1351::RGB(0, 128, 255);
 
-  for (int i = 0; i < particles; i++) {
-    offsets[i] += random(-2, 3);
-    display.drawLine(x_pos[i] + offsets[i], y_pos[i] + offsets[i], pos,
-                     80 + sin(pos / 4.0) * 20, particle_colors[i]);
-    display.drawCircle(x_pos[i] + offsets[i], y_pos[i] + offsets[i], 1,
-                       circleColor);
-  }
-  display.updateScreen();
-  Serial.println(millis() - before);
-
-  if (up) {
-    pos++;
-    if (pos >= 127) {
-      up = false;
-    }
-  } else {
-    pos--;
-    if (pos < 0) {
-      up = true;
-    }
-  }
-
-  // Send bytes from ESP8266 to computer
-  if (Serial1.available() > 0) {
-    Serial.write(Serial1.read());
-  }
-
-  // Send bytes from computer back to ESP8266
-  if (Serial.available() > 0) {
-    Serial1.write(Serial.read());
-  }
-
+void _loop_interactionSD() {
   // send data only when you receive data:
   if (Serial.available() > 0)
   {
@@ -172,4 +180,35 @@ void loop() {
   {
     continueRecording();
   }
+}
+
+void _loop_interactionWifi() {
+  // Send bytes from ESP8266 to computer
+  if (Serial1.available() > 0)
+  {
+    Serial.write(Serial1.read());
+  }
+
+  // Send bytes from computer back to ESP8266
+  if (Serial.available() > 0)
+  {
+    Serial1.write(Serial.read());
+    // DEBUG
+    String s = Serial.readString();
+    if (s.startsWith("m")) {
+      String c = s.substring(1, 2);
+      int channel = c.toInt();
+
+      String amt = s.substring(3, 4);
+      float gain = amt.toFloat();
+      Serial.println("gain received -->");
+      controlGrain(channel, gain);
+    }
+
+  }
+}
+
+void controlGrain (int channel, float value) {
+  // channel must be between 0 and 3
+  mixerL.gain(channel, value);
 }
